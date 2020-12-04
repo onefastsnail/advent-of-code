18d2922e3f115900d8b6250faf829f9a12ea81d9
Paul Stewart <onefastsnail@gmail.com>
2020-12-04 12:38:11 +0200

    Day 2 part 1

---
+++ 2020/tests/Day2Test.elm
@@ -0,0 +1,25 @@
+module Day2Test exposing (..)
+
+import Day2 exposing (..)
+import Expect exposing (..)
+import Test exposing (..)
+
+
+puzzleInput : String
+puzzleInput =
+    "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc"
+
+
+suite : Test
+suite =
+    describe "Day2"
+        [ test "parsePuzzleInput"
+            (\_ ->
+                Day2.parsePuzzleInput puzzleInput
+                    |> List.length
+                    |> Expect.equal 3
+            )
+        , test
+            "getAnswerPart1"
+            (\_ -> Day2.getAnswerPart1 puzzleInput |> Expect.equal 2)
+        ]
