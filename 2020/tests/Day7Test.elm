36c51bf5652bae8e8d3fe8b70b49b194854db453
Paul Stewart <onefastsnail@gmail.com>
2020-12-26 21:18:53 +0200

    Day 7 part 1

---
+++ 2020/tests/Day7Test.elm
@@ -0,0 +1,26 @@
+module Day7Test exposing (..)
+
+import Day7 exposing (..)
+import Dict
+import Expect exposing (..)
+import Test exposing (..)
+
+
+puzzleInput : String
+puzzleInput =
+    "light red bags contain 1 bright white bag, 2 muted yellow bags.\ndark orange bags contain 3 bright white bags, 4 muted yellow bags.\nbright white bags contain 1 shiny gold bag.\nmuted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\nshiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\ndark olive bags contain 3 faded blue bags, 4 dotted black bags.\nvibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\nfaded blue bags contain no other bags.\ndotted black bags contain no other bags."
+
+
+suite : Test
+suite =
+    describe "Day7"
+        [ test "parsePuzzleInput"
+            (\_ ->
+                Day7.parsePuzzleInput puzzleInput
+                    |> Dict.size
+                    |> Expect.equal 9
+            )
+        , test
+            "getAnswerPart1 1"
+            (\_ -> Day7.getAnswerPart1 puzzleInput |> Expect.equal 4)
+        ]
