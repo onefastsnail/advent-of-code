a1efe2a4da361a7a1893adc932d6e57b8eeb4a1b
Paul Stewart <onefastsnail@gmail.com>
2021-01-01 12:53:55 +0200

    Starting Day 10 part 2

--- 2020/src/Day10.elm
+++ 2020/src/Day10.elm
@@ -1,6 +1,7 @@
 module Day10 exposing (..)
 
 import Array
+import List
 
 
 puzzleInput : String
@@ -13,7 +14,7 @@ parsePuzzleInput str =
     String.split "\n" str |> List.map (\a -> String.toInt a |> Maybe.withDefault 0)
 
 
-connectAdapters : List Int -> List Int -> List Int -> List Int
+connectAdapters : List Int -> List Int -> List ( Int, Int ) -> List ( Int, Int )
 connectAdapters connectedAdapters freeAdapters differences =
     case freeAdapters of
         [] ->
@@ -28,7 +29,7 @@ connectAdapters connectedAdapters freeAdapters differences =
                     List.head freeAdapters |> Maybe.withDefault 0
 
                 difference =
-                    nextAdapter - currentAdapter
+                    ( currentAdapter, nextAdapter - currentAdapter )
             in
             connectAdapters ([ nextAdapter ] ++ connectedAdapters) (List.drop 1 freeAdapters) (differences ++ [ difference ])
 
@@ -40,10 +41,96 @@ getAnswerPart1 puzzle =
         |> (\differences ->
                 let
                     x =
-                        List.filter (\a -> a == 1) differences |> List.length
+                        List.filter (\( _, a ) -> a == 1) differences |> List.length
 
                     y =
-                        List.filter (\a -> a == 3) differences |> List.length
+                        List.filter (\( _, a ) -> a == 3) differences |> List.length
                 in
                 x * (y + 1)
            )
+
+
+puzzleInput1 =
+    "16\n10\n15\n5\n1\n11\n7\n19\n6\n12\n4"
+
+
+puzzleInput2 =
+    "28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3"
+
+
+
+-- findArrangements adapters
+-- getAnswerPart2 puzzle =
+--     let
+--         numbers =
+--             parsePuzzleInput puzzle |> List.sort |> Array.fromList
+--         differences =
+--             parsePuzzleInput puzzle |> (\adapters -> connectAdapters [] (List.sort adapters) [])
+--         ones =
+--             List.filter (\( _, d ) -> d == 1) differences |> Debug.log "x"
+--     in
+--     List.foldl
+--         (\( k, v ) a ->
+--             let
+--                 start =
+--                     Array.get k numbers |> Maybe.withDefault 999 |> Debug.log "a"
+--                 end =
+--                     Array.get (k + 2) numbers |> Maybe.withDefault 111 |> Debug.log "b"
+--                 canNextBeRemoved =
+--                     end - adapter <= 3 |> Debug.log "c"
+--             in
+--             case canNextBeRemoved of
+--                 True ->
+--                     a + 1
+--                 False ->
+--                     a
+--         )
+--         1
+--         (List.indexedMap Tuple.pair (Array.toList numbers))
+
+
+getAnswerPart2 puzzle =
+    let
+        numbers =
+            parsePuzzleInput puzzle
+                |> List.append [ 0 ]
+                |> List.sort
+                |> (\a -> List.append a [ List.reverse a |> List.head |> Maybe.withDefault 0 |> (\b -> b + 3) ])
+
+        indexedNumbers =
+            Array.fromList numbers
+    in
+    List.foldl
+        (\( k, _ ) a ->
+            let
+                adapter =
+                    Array.get k indexedNumbers |> Maybe.withDefault 0 |> Debug.log "a"
+
+                slice =
+                    Array.slice (k + 1) (k + 4) indexedNumbers |> Array.toList |> Debug.log "b"
+
+                x =
+                    List.foldl
+                        (\nextAdapter count ->
+                            let
+                                canNextBeRemoved =
+                                    nextAdapter - adapter < 3 |> Debug.log "c"
+                            in
+                            case canNextBeRemoved of
+                                True ->
+                                    count + 1
+
+                                False ->
+                                    count
+                        )
+                        0
+                        slice
+                        |> Debug.log "d"
+            in
+            x :: a
+        )
+        []
+        (List.indexedMap Tuple.pair numbers)
+        |> List.filter (\a -> a > 0)
+        |> Debug.log "e"
+        |> List.product
