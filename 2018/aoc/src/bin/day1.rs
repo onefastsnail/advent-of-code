ccd516fc12c958ce2edc7dc355f2224658c93188
Paul Stewart <onefastsnail@gmail.com>
2022-10-10 15:55:08 +0300

    Day 2 part 1 and cleaning things up

--- 2018/aoc/src/bin/day1.rs
+++ 2018/aoc/src/bin/day1.rs
@@ -1,43 +1,37 @@
 use std::collections::HashSet;
 
 fn main() {
-    let answer = part1();
+    let changes = std::fs::read_to_string("./inputs/day1.txt")
+        .expect("file not found!")
+        .lines()
+        .map(|it| it.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let answer = part1(&changes);
     println!("Part 1 answer is: {}", answer);
 
-    let answer = part2();
+    let answer = part2(&changes);
     println!("Part 2 answer is: {}", answer);
 }
 
-fn part1() -> i32 {
-    let changes = std::fs::read_to_string("./inputs/day1.txt")
-        .expect("file not found!")
-        .lines()
-        .map(|it| it.parse::<i32>().unwrap())
-        .collect::<Vec<i32>>();
-
+fn part1(changes: &Vec<i32>) -> i32 {
     let mut frequency = 0;
-    for change in &changes {
+    for change in changes {
         frequency += change;
     }
 
-    return frequency;
+    frequency
 }
 
-fn part2() -> i32 {
-    let changes = std::fs::read_to_string("./inputs/day1.txt")
-        .expect("file not found!")
-        .lines()
-        .map(|it| it.parse::<i32>().unwrap())
-        .collect::<Vec<i32>>();
-
+fn part2(changes: &Vec<i32>) -> i32 {
     let mut frequency = 0;
-    let mut seen = HashSet::new();
+    let mut seen = HashSet::new(); // Using a HashSet over a Vector was significantly faster
     let mut found = false;
 
     seen.insert(0);
 
-    while found != true {
-        for change in &changes {
+    while !found {
+        for change in changes {
             frequency += change;
 
             if seen.contains(&frequency) {
@@ -49,5 +43,30 @@ fn part2() -> i32 {
         }
     }
 
-    return frequency;
+    frequency
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_solves_part1_example1() {
+        assert_eq!(part1(&Vec::from([1, -2, 3, 1])), 3);
+    }
+
+    #[test]
+    fn it_solves_part1_example2() {
+        assert_eq!(part1(&Vec::from([1, 1, -2])), 0);
+    }
+
+    #[test]
+    fn it_solves_part2_example1() {
+        assert_eq!(part2(&Vec::from([3, 3, 4, -2, -4])), 10);
+    }
+
+    #[test]
+    fn it_solves_part2_example2() {
+        assert_eq!(part2(&Vec::from([7, 7, -2, -7, -4])), 14);
+    }
 }
--- 2018/aoc/src/bin/day2.rs
+++ 2018/aoc/src/bin/day2.rs
@@ -1,4 +1,63 @@
+use std::collections::HashMap;
+
 fn main() {
-    let four = "-4".parse::<i32>();
-    println!("{:?}", four);
+    let input = std::fs::read_to_string("./inputs/day2.txt").expect("file not found!");
+
+    let answer = part1(&input);
+    println!("Part 1 answer is: {}", answer);
+
+    let answer = part2(&input);
+    println!("Part 2 answer is: {}", answer);
+}
+
+fn part1(input: &str) -> i32 {
+    let lines = input
+        .lines()
+        .map(|it| it.split("").filter(|&i| !i.is_empty()));
+
+    let mut twos: i32 = 0;
+    let mut threes: i32 = 0;
+
+    for characters in lines {
+        let mut frequencies = HashMap::new();
+
+        for s in characters {
+            *frequencies.entry(s).or_insert(0) += 1
+        }
+
+        if frequencies.iter().any(|(_, v)| *v == 2) {
+            twos += 1;
+        }
+
+        if frequencies.iter().any(|(_, v)| *v == 3) {
+            threes += 1;
+        }
+    }
+
+    twos * threes
+}
+
+fn part2(input: &str) -> String {
+    input.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_solves_part1_example1() {
+        assert_eq!(
+            part1("abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab"),
+            12
+        );
+    }
+
+    #[test]
+    fn it_solves_part2_example1() {
+        assert_eq!(
+            part2("abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz"),
+            "fgij".to_string()
+        );
+    }
 }
