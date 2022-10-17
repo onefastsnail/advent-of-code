1563e4d8f57c63fe215f68245c1985b9fe695a9e
Paul Stewart <onefastsnail@gmail.com>
2022-10-17 16:57:08 +0300

    Day 2 part 2

--- 2018/src/bin/day2.rs
+++ 2018/src/bin/day2.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("Part 1 answer is: {}", answer);
 
     let answer = part2(&input);
-    println!("Part 2 answer is: {}", answer);
+    println!("Part 2 answer is: {}", answer.unwrap());
 }
 
 fn part1(input: &str) -> i32 {
@@ -37,8 +37,37 @@ fn part1(input: &str) -> i32 {
     twos * threes
 }
 
-fn part2(input: &str) -> String {
-    input.to_string()
+fn part2(input: &str) -> Result<String, String> {
+    let lines = input
+        .lines()
+        .map(|it| it.split("").collect::<Vec<&str>>())
+        .collect::<Vec<Vec<&str>>>();
+
+    let matches_needed = lines[0].len() - 1;
+
+    let mut matches: Vec<String> = Vec::new();
+
+    let cloned_lines = lines.clone();
+
+    for characters in &lines {
+        for other_characters in &cloned_lines {
+            let matching = characters
+                .iter()
+                .zip(other_characters)
+                .filter(|&(a, b)| **a == **b)
+                .collect::<Vec<_>>();
+
+            if matching.len() == matches_needed {
+                let x = matching.iter().map(|&(_, b)| *b).collect::<_>();
+                matches.push(x);
+            }
+        }
+    }
+
+    match matches.len() == 2 {
+        true => Ok(matches[0].to_string()),
+        _ => Err("Didn't find 2 matches".into()),
+    }
 }
 
 #[cfg(test)]
@@ -54,10 +83,22 @@ mod tests {
     }
 
     #[test]
+    fn it_solves_part1() {
+        let input = std::fs::read_to_string("./inputs/day2.txt").expect("file not found!");
+        assert_eq!(part1(&input), 6000);
+    }
+
+    #[test]
     fn it_solves_part2_example1() {
         assert_eq!(
-            part2("abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz"),
+            part2("abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz").unwrap(),
             "fgij".to_string()
         );
+    }
+
+    #[test]
+    fn it_solves_part2() {
+        let input = std::fs::read_to_string("./inputs/day2.txt").expect("file not found!");
+        assert_eq!(part2(&input).unwrap(), "pbykrmjmizwhxlqnasfgtycdv");
     }
 }
