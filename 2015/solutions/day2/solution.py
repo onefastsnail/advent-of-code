2fca79c3b20f916d32ac418df47664fd297a1d44
onefastsnail <onefastsnail@gmail.com>
2024-08-11 16:19:14 +0300

    Adding Ruff linting

--- 2015/solutions/day2/solution.py
+++ 2015/solutions/day2/solution.py
@@ -5,11 +5,11 @@ def part_1(raw_input: List[str]) -> int:
     total = 0
 
     for line in raw_input:
-        l, w, h = list(map(lambda x: int(x), line.split("x")))
+        length, width, height = list(map(lambda x: int(x), line.split("x")))
 
-        s1 = l * w
-        s2 = w * h
-        s3 = h * l
+        s1 = length * width
+        s2 = width * height
+        s3 = height * length
 
         total = total + s1 * 2 + s2 * 2 + s3 * 2 + min(s1, s2, s3)
 
@@ -20,12 +20,12 @@ def part_2(raw_input: List[str]) -> int:
     total = 0
 
     for line in raw_input:
-        l, w, h = list(map(lambda x: int(x), line.split("x")))
+        length, width, height = list(map(lambda x: int(x), line.split("x")))
 
-        volume = l * w * h
-        s1 = 2 * (l + w)
-        s2 = 2 * (w + h)
-        s3 = 2 * (h + l)
+        volume = length * width * height
+        s1 = 2 * (length + width)
+        s2 = 2 * (width + height)
+        s3 = 2 * (height + length)
 
         total = total + volume + min(s1, s2, s3)
 
