2431491ad4078ba83285fc11419b72cd1e317973
onefastsnail <onefastsnail@gmail.com>
2024-12-11 18:27:11 +0200

    2024 day 3 part 2

--- 2024/src/bin/day3.rs
+++ 2024/src/bin/day3.rs
@@ -8,20 +8,33 @@ fn main() {
 }
 
 fn part1(input: &str) -> i32 {
-    let re = Regex::new(r"mul\((?<x>\d+),(?<y>\d+)\)").unwrap();
-
-    let mul_pairs = re.captures_iter(input).map(|caps| {
-        let (_, [x, y]) = caps.extract();
-        let a = x.parse::<i32>().unwrap();
-        let b = y.parse::<i32>().unwrap();
-        (a, b)
-    });
-
-    mul_pairs.fold(0, |acc, (x, y)| acc + (x * y))
+    sum_instructions(input)
 }
 
 fn part2(input: &str) -> i32 {
-    0
+    let (start, _) = input.split_once("don't()").unwrap();
+    let between_re = Regex::new(r"do\(\)(?<seq>.*?)(don't\(\)|$)").unwrap();
+
+    let sequences = between_re
+        .captures_iter(input)
+        .filter_map(|caps| caps.name("seq").map(|m| m.as_str()));
+
+    sequences.fold(sum_instructions(start), |accum, sequence| {
+        accum + sum_instructions(sequence)
+    })
+}
+
+fn sum_instructions(input: &str) -> i32 {
+    let re = Regex::new(r"mul\((?<x>\d+),(?<y>\d+)\)").unwrap();
+
+    re.captures_iter(input)
+        .map(|caps| {
+            let (_, [x, y]) = caps.extract();
+            let a = x.parse::<i32>().unwrap();
+            let b = y.parse::<i32>().unwrap();
+            a * b
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -44,15 +57,15 @@ mod tests {
 
     #[test]
     fn it_solves_part2_example1() {
-        let input = std::fs::read_to_string("./inputs/day2/teaser.txt").expect("file not found!");
+        let input = std::fs::read_to_string("./inputs/day3/teaser.txt").expect("file not found!");
 
         assert_eq!(part2(&input), 48);
     }
 
     #[test]
     fn it_solves_part2() {
-        let input = std::fs::read_to_string("./inputs/day2/input.txt").expect("file not found!");
+        let input = std::fs::read_to_string("./inputs/day3/input.txt").expect("file not found!");
 
-        assert_eq!(part2(&input), 426);
+        assert_eq!(part2(&input), 90669332);
     }
 }
